3bd58bbfa704c3d9b2d7ee45e9bb32e8327c6292
Malte Siemers <malte.siemers@mailbox.org>
2023-10-01 18:48:26 +0200

    adjust manifest, add code for scheme

--- generate.jl
+++ generate.jl
@@ -1,1 +1,25 @@
-#!/bin/bash
+using Pkg
+Pkg.activate( "PlotEnv")
+Pkg.instantiate()
+
+using RNASeqTools, BioSequences, BioAlignments, CairoMakie, BioGenerics, DelimitedFiles, GeometryTypes, CSV, JLD2, DataFrames, StatsBase, Combinatorics
+
+struct Interactions
+    nodes::DataFrame
+    edges::DataFrame
+    edgestats::Dict{Tuple{Int,Int}, Tuple{Int, Dict{Tuple{Int,Int},Int}, Dict{Tuple{Int,Int},Int}}}
+    bpstats::Dict{Tuple{Int,Int}, Tuple{Float64, Int64, Int64, Int64, Int64, Float64}}
+    multichimeras::Dict{Vector{Int}, Int}
+    replicate_ids::Vector{Symbol}
+    counts::Dict{Symbol,Vector{Int}}
+end
+
+Interactions(filepath::String) = jldopen(filepath,"r"; typemap=Dict("ChimericAnalysis.Interactions" => Interactions)) do f
+    f["interactions"]
+end
+
+data_folder = joinpath(@__DIR__, "data")
+source_folder = joinpath(@__DIR__, "src")
+
+include(joinpath(source_folder, "figure_1.jl"))
+plot_figure_1(joinpath(data_folder, "figure_1"))
---
+++ src/figure_1.jl
@@ -0,0 +1,47 @@
+function plot_figure_1(assets_dir::String)
+    resfactor = 1.0
+    fig = Figure(resolution=(1200*resfactor, 800*resfactor))
+
+    ga = fig[1:2,1] = GridLayout()
+
+    ax1 = Axis(ga[1, 1], leftspinevisible = false, rightspinevisible = false, bottomspinevisible = false, topspinevisible = false, aspect = DataAspect())
+    img1 = rotr90(load(joinpath(assets_dir, "1A.png")))
+    hidedecorations!(ax1)
+    image!(ax1, img1, aspect = DataAspect())
+
+    ax2 = Axis(ga[1, 2], leftspinevisible = false, rightspinevisible = false, bottomspinevisible = false, topspinevisible = false, aspect = DataAspect())
+    img2 = rotr90(load(joinpath(assets_dir, "1B.png")))
+    hidedecorations!(ax2)
+    image!(ax2, img2, aspect = DataAspect())
+
+    ax3 = Axis(ga[1, 3], leftspinevisible = false, rightspinevisible = false, bottomspinevisible = false, topspinevisible = false, aspect = DataAspect())
+    img3 = rotr90(load(joinpath(assets_dir, "1C.png")))
+    hidedecorations!(ax3)
+    image!(ax3, img3, aspect = DataAspect())
+
+    gb = fig[3:5,1] = GridLayout()
+
+    ax4 = Axis(gb[1, 1], leftspinevisible = false, rightspinevisible = false, bottomspinevisible = false, topspinevisible = false, aspect = DataAspect())
+    img4 = rotr90(load(joinpath(assets_dir, "1D.png")))
+    hidedecorations!(ax4)
+    image!(ax4, img4, aspect = DataAspect())
+
+    ax5 = Axis(gb[1, 2], leftspinevisible = false, rightspinevisible = false, bottomspinevisible = false, topspinevisible = false, aspect = DataAspect())
+    img5 = rotr90(load(joinpath(assets_dir, "1E.png")))
+    hidedecorations!(ax5)
+    image!(ax5, img5, aspect = DataAspect())
+
+    ax6 = Axis(gb[1, 3], leftspinevisible = false, rightspinevisible = false, bottomspinevisible = false, topspinevisible = false, aspect = DataAspect())
+    img6 = rotr90(load(joinpath(assets_dir, "1F.png")))
+    hidedecorations!(ax6)
+    image!(ax6, img6, aspect = DataAspect())
+
+    Label(ga[1,1, TopLeft()], "A", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
+    Label(ga[1,2, TopLeft()], "B", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
+    Label(ga[1,3, TopLeft()], "C", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
+    Label(gb[1,1, TopLeft()], "D", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
+    Label(gb[1,2, TopLeft()], "E", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
+    Label(gb[1,3, TopLeft()], "F", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
+
+    save("figure_1.svg", fig)
+end
