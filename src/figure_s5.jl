5c282b9ac6477ea1c72726dc0f7c360eac06ea90
Malte Siemers <malte.siemers@mailbox.org>
2023-12-05 17:12:42 +0100

    new panel

--- src/figure_s5.jl
+++ src/figure_s5.jl
@@ -81,27 +81,30 @@ function plot_figure_s5(interact::InteractionsNew, interact2::InteractionsNew;
         bp_interval_len=30, fdr_cut=0.25, probmass_min=0.3, probmass_max=0.7,
         min_partners=3, min_ligpoints=10, connect_within=1)
     resfactor = 1.
-    fig = Figure(resolution=(1200*resfactor, 1200*resfactor))
+    fig = Figure(resolution=(1200*resfactor, 1600*resfactor))
 
-    ax_pos = Axis(fig[3, 2], title="position in chimera", ylabel="count",
+    ax_runtime = Axis(fig[1, 1], title="Runtime comparison", ylabel="runtime [s / Mio. reads]", yticks=(0:3, ["1", "10", "100", "1000"]),
+        xticks = (1:2, ["excluding mapping", "total"]))
+
+    ax_pos = Axis(fig[4, 1], title="position in chimera", ylabel="count",
         xticks = (1:4, ["RNA1,\nFDR <= 0.05", "RNA1,\nno FDR", "RNA2,\nFDR <= 0.05", "RNA2,\nno FDR"]))
-    ax_partner = Axis(fig[2, 1], title="partners of sRNAs", ylabel="# of partners", xticks = (1:2, ["RNA1", "RNA2"]))
+    ax_partner = Axis(fig[2, 2], title="partners of sRNAs", ylabel="# of partners", xticks = (1:2, ["RNA1", "RNA2"]))
 
-    ax_regions_s = Axis(fig[2, 2], title="regions in RNA1 sRNAs", ylabel="count", xlabel="# of regions", xticks = 1:100, yticks=2:2:10)
-    ax_regions = Axis(fig[3, 1], title="regions in RNA2 sRNAs", ylabel="count", xlabel="# of regions", xticks = 1:100)
+    ax_regions_s = Axis(fig[3, 1], title="regions in RNA1 sRNAs", ylabel="count", xlabel="# of regions", xticks = 1:100, yticks=2:2:10)
+    ax_regions = Axis(fig[3, 2], title="regions in RNA2 sRNAs", ylabel="count", xlabel="# of regions", xticks = 1:100)
 
-    ax_graph = Axis(fig[1, 1], title="OppZ basepairing", ylabel="count / 10^3", xlabel="position")
+    ax_graph = Axis(fig[1, 2], title="OppZ basepairing", ylabel="count / 10^3", xlabel="position")
     aggregation_plot!(ax_graph, interact, "OppZ", fdr_cut; norm=1000)
-    #axislegend(ax_graph)
+    axislegend(ax_graph)
 
-    ax_graph2 = Axis(fig[1, 2], title=rich(rich("oppB"; font=:bold_italic), rich(" basepairing")),
+    ax_graph2 = Axis(fig[2, 1], title=rich(rich("oppB"; font=:bold_italic), rich(" basepairing")),
         ylabel="count", xlabel="position")
     aggregation_plot!(ax_graph2, interact, "oppB", fdr_cut)
-    #axislegend(ax_graph2)
+    axislegend(ax_graph2)
 
     labels = ["RNA1", "RNA2"]
     elements = [PolyElement(polycolor = i) for i in (RGBAf(0.388, 0.431, 0.98, 0.6), RGBAf(0.937, 0.333, 0.231, 0.6))]
-    Legend(fig[1, 3], elements, labels)
+    #Legend(fig[1, 3], elements, labels)
 
     srnas1 = findall(interact.nodes.type .== "sRNA")
     srnas2 = findall(interact2.nodes.type .== "sRNA")
@@ -123,6 +126,18 @@ function plot_figure_s5(interact::InteractionsNew, interact2::InteractionsNew;
         min_ligpoints=min_ligpoints, connect_within=connect_within, fisher_fdr=0.05)
 
     colors = Makie.wong_colors()
+
+    barplot!(ax_runtime, [1,1,2,2], log10.([3, 1140, 10, 1290]), dodge=[1,2,1,2],
+        color=colors[[3,4,3,4]], bar_labels = :y, label_formatter = x->"$(Int(floor(10^x, digits=0)))")
+    ylims!(ax_runtime, (-0.1,3.8))
+    xlims!(ax_runtime, (0.3,2.5))
+    labels = ["Chimeric\nFragments", "RNAnue"]
+    elements = [PolyElement(polycolor = colors[i]) for i in 3:4]
+    axislegend(ax_runtime, elements, labels, position=:lt)
+    #for (t, i,j) in [(5.0, 0.8, log10(5.0)+0.2),(1200.0, 1.2, log10(1200.0)+0.2),(5.5, 1.8, log10(5.5)+0.2),(1400.0, 2.2, log10(1400.0)+0.2)]
+    #    text!(ax_runtime, string(t), position = (i,j), align = (:center, :center))
+    #end
+
     mean_partners = [mean(counter_partners[is_sponge]), mean(counter_partners2[is_sponge2]), mean(counter_partners[.!is_sponge]), mean(counter_partners2[.!is_sponge2])]
     sd_partners = [std(counter_partners[is_sponge]), std(counter_partners2[is_sponge2]), std(counter_partners[.!is_sponge]), std(counter_partners2[.!is_sponge2])]
 
@@ -141,7 +156,7 @@ function plot_figure_s5(interact::InteractionsNew, interact2::InteractionsNew;
 
     labels = ["LCD", "HCD"]
     elements = [PolyElement(polycolor = colors[i]) for i in 1:length(labels)]
-    #axislegend(ax_partner, elements, labels, position=:lt)
+    axislegend(ax_partner, elements, labels, position=:lt)
 
     count_sponge = [sum(is_sponge_fisher), sum(is_sponge2_fisher), sum(is_sponge), sum(is_sponge2),
                     sum(.!is_sponge_fisher), sum(.!is_sponge2_fisher), sum(.!is_sponge), sum(.!is_sponge2)]
@@ -151,7 +166,7 @@ function plot_figure_s5(interact::InteractionsNew, interact2::InteractionsNew;
 
     barplot!(ax_pos, exp, count_sponge, dodge=groups, color=colors[groups])
 
-    #axislegend(ax_pos, elements, labels, position=:lt)
+    axislegend(ax_pos, elements, labels, position=:lt)
 
     n_s = max(maximum(counter_regions[is_sponge, 1]), maximum(counter_regions2[is_sponge2, 1]))
 
@@ -164,7 +179,7 @@ function plot_figure_s5(interact::InteractionsNew, interact2::InteractionsNew;
     regions_groups_s = vcat(repeat([2], n_s), repeat([1], n_s))
 
     barplot!(ax_regions_s, vcat(1:n_s, 1:n_s), count_regions_s, dodge=regions_groups_s, color=colors[regions_groups_s])
-    #axislegend(ax_regions_s, elements, labels, position=:rt)
+    axislegend(ax_regions_s, elements, labels, position=:rt)
 
     n = max(maximum(counter_regions[.!is_sponge, 2]), maximum(counter_regions2[.!is_sponge2, 2]))
 
@@ -176,9 +191,9 @@ function plot_figure_s5(interact::InteractionsNew, interact2::InteractionsNew;
     regions_groups = vcat(repeat([2], n), repeat([1], n))
 
     barplot!(ax_regions, vcat(1:n, 1:n), count_regions, dodge=regions_groups, color=colors[regions_groups])
-    #axislegend(ax_regions, elements, labels, position=:rt)
+    axislegend(ax_regions, elements, labels, position=:rt)
 
-    Legend(fig[2:3, 3], elements, labels)
+    #Legend(fig[2:3, 3], elements, labels)
 
     Label(fig[1,1, TopLeft()], "a", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
     Label(fig[1,2, TopLeft()], "b", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
@@ -186,6 +201,7 @@ function plot_figure_s5(interact::InteractionsNew, interact2::InteractionsNew;
     Label(fig[2,2, TopLeft()], "d", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
     Label(fig[3,1, TopLeft()], "e", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
     Label(fig[3,2, TopLeft()], "f", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
+    Label(fig[4,1, TopLeft()], "g", fontsize = 26, font = :bold, padding = (0, 5, 5, 0), halign = :right)
 
     save("figure_E7.pdf", fig)
     save("figure_E7.png", fig, px_per_unit = 2)
